3b6410ae2b97488c1d0ad9f8443bd9337328564b
Carson Crow <serranocrow@pm.me>
2023-01-12 16:06:00 -0600

    initial notes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIA7CQvtNyhqQoJwnebpUZOJox2fUtFvo",
+  authDomain: "learning-firebase-d68ec.firebaseapp.com",
+  projectId: "learning-firebase-d68ec",
+  storageBucket: "learning-firebase-d68ec.appspot.com",
+  messagingSenderId: "372759185105",
+  appId: "1:372759185105:web:a5432352a493ad7145a7f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
